fef735d4c0b7375df6e7d7c1eb88607f00db9922
alaguia-del <al.aguia@gmail.com>
2026-01-29 15:02:33 +0100

    Update app_cloud.py

--- app_cloud.py
+++ app_cloud.py
@@ -1,5 +1,4 @@
 import random
-import time
 import os
 import threading
 from datetime import datetime
@@ -70,7 +69,7 @@ thread.start()
 
 @app.route('/latest', methods=['GET'])
 def get_latest():
-        print(f"Request: {latest_signal['Asset']} | Type: {latest_signal['type']}", flush=True)
+    print(f"Request: {latest_signal['Asset']} | Type: {latest_signal['type']}", flush=True)
     return jsonify(latest_signal)
 
 @app.route('/', methods=['GET'])
@@ -80,4 +79,5 @@ def health_check():
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
+
 
